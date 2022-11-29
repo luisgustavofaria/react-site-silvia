038820e72e7446d0cd0f745c51101b69b0dee7fc
Luis Gustavo Faria <luis.gustavo.fariac@gmail.com>
2022-11-29 20:56:39 -0300

    Token de acesso

--- pages/api/user/signup.js
+++ pages/api/user/signup.js
@@ -1,27 +1,37 @@
-import Joi from 'joi' 
-import createHandler from '../../../lib/middlewares/nextConnect'
+import Joi from 'joi'
+import { withIronSessionApiRoute } from 'iron-session/next'
 
-import { signupUser } from "../../../modules/user/user.service"
+import createHandler from '../../../lib/middlewares/nextConnect'
 import validate from '../../../lib/middlewares/validation'
+import { signupUser } from '../../../modules/user/user.service'
+
+import { ironConfig } from '../../../lib/middlewares/ironSession'
 
 const postSchema = Joi.object({
-    firstName: Joi.string().required().max(50),
-    lastName: Joi.string().required().max(50),
-    user: Joi.string().required().max(30),
-    email: Joi.string().email().required().max(100),
-    password: Joi.string().required().max(50).min(6),
-  })
+  firstName: Joi.string().required().max(50),
+  lastName: Joi.string().required().max(50),
+  user: Joi.string().required().max(30),
+  email: Joi.string().email().required().max(100),
+  password: Joi.string().required().max(50).min(6),
+})
 
 const signup = createHandler()
-    
+
 signup.post(validate({ body: postSchema }), async (req, res) => {
   try {
     const user = await signupUser(req.body)
-    res.status(201).json(user)
+
+    req.session.user = {
+      id: user._id,
+      user: user.user
+    }
+    await req.session.save()
+
+    res.status(201).json({ ok: true })
   } catch (err) {
     console.error(err)
     throw err
   }
 })
 
-export default signup
+export default withIronSessionApiRoute(signup, ironConfig)
